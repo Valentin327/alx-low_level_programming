686b9aca13ee03507f9bf2994d3dfad7957fef4c
ValentinZONGO <valentizongo@gmail.com>
2022-04-07 14:06:52 -0700

    C Data type

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longIntType;
+        long long int longIntType;
+	float floatType;
+
+
+	// sizeof evaluates the size of a variable
+	printf("Size of a char: %zu byte(s)\n", sizeof(charTYpe));
+	printf("size of a int:  %zu byte(s)\n", sizeof(intType));
+	printf("size of a long int: %zu byte(s)\n", sizeof(longIntType));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
+	printf("size of a float: %zu byte(s)\n", sizeof(floatType));
+	
+	return (0)
+}
+
