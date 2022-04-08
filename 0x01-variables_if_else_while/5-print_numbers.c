58069643f37d664e56b7f70d45cdf74eeb59ebab
ValentinZONGO <valentizongo@gmail.com>
2022-04-08 14:51:22 -0700

    numbers:first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alpha a-z but remove 'q' and 'e'
+ * 
+ *
+ * Return: Always 0 (Success)  
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i=0; i<=10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
+
