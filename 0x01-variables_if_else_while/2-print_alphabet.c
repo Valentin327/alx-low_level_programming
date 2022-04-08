64eccbb76afce56983611772cee099612b9be53e
ValentinZONGO <valentizongo@gmail.com>
2022-04-08 10:52:03 -0700

    Print alphabet:first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>  
+/* more headers goes there */  
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main (void)
+{
+	int count = 97;
+
+	while(count <= 122)
+	{
+		putchar(count);
+		count++;
+	}
+	return (0);
+}
