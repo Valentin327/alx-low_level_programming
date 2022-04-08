d21a05f36983896af7a90876fef21e30e2283678
ValentinZONGO <valentizongo@gmail.com>
2022-04-08 09:11:21 -0700

    compare random number:second commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,6 @@
  *
  *Return: Always 0 (Success)
 */
-
 int main(void)
 {
   int n;
@@ -21,7 +20,7 @@ int main(void)
   /* your code goes there */
   lastDigit = n % 10;
   if (lastDigit > 5)
-    printf("Last digit of % d is %d and is greater than 5\n", n, lastDigit);       
+    printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);       
   else if (lastDigit == 0)
     printf("Last digit of %d is %d and is 0\n", n, lastDigit);
   else if (lastDigit < 6)
