af045259b094a2fa0644e5c054fc087f6b74b40c
ValentinZONGO <valentizongo@gmail.com>
2022-04-09 06:25:37 -0700

    Combination of all digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	n++;
+	}
+	return (0);
+}
+
