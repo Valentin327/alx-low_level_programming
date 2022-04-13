a88bc55d1c6c9ac313c35b9a456164502ce8b64e
ValentinZONGO <valentizongo@gmail.com>
2022-04-13 13:39:56 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - function to add two integer
+ *@a: first integer
+ *@b: second integer
+ *Return: result of two integer
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
