44beaf711af7a8c31eed236f2cfa77aa9292c05a
ValentinZONGO <valentizongo@gmail.com>
2022-04-13 09:35:24 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - A function that prints the sign of a number
+ * @n: the number to check
+ * Return: 1 if is greater than 0, 0 if is zero and -1 otherwise
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else
+	{
+		return (-1);
+		_putchar('-');
+	}
