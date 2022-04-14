b944df88d269439da131cfab187c1bb9deb24675
ValentinZONGO <valentizongo@gmail.com>
2022-04-14 14:19:14 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - function that checks for uppercase character.
+ *
+ * @c: the ascii code of character to check
+ *
+ * Return: return 0 or 1 if upper character
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
