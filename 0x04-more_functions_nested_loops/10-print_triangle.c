0963fb603306d8872a6c405c749a199bb113f082
ValentinZONGO <valentizongo@gmail.com>
2022-04-15 07:03:59 -0700

    third commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,8 +12,11 @@ void print_triangle(int size)
 	_putchar('\n');
 	for (i = 0; i <= size; i++)
 	{
-		for (j = 0; j <= i; j++)
+		for (j = 0; j <= size; j++)
 		{
+			if(j <= (size - i)
+			_putchar(' ');
+			else
 			_putchar('#');
 		}
 		_putchar('\n');
