a14e9a9c0e8adfe9177b343e4fa412a78eec8dfb
ValentinZONGO <valentizongo@gmail.com>
2022-04-14 21:56:31 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers
+ *
+ * Return: void
+ *
+*/
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
