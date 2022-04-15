b9f1f17d4cbe71a53a7eb3b2ecc0c0c1ae6646df
ValentinZONGO <valentizongo@gmail.com>
2022-04-15 06:27:12 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_triangle - function that prints a triangle
+ * @size: the size of the triangle
+ * Return: no return
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	_putchar('\n');
+	for (i = 0; i <= size; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
