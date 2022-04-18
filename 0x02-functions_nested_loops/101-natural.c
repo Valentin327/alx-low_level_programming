7ca10daa8cf535f1a6ec1721cb4bc4c795871b59
ValentinZONGO <valentizongo@gmail.com>
2022-04-18 10:52:26 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/** more headers goes there */
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int sum, i;
+	sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
