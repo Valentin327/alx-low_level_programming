10be75f20506f8ff0dc3fd490c9637525976ae1d
ValentinZONGO <valentizongo@gmail.com>
2022-04-18 08:38:59 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/** more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int t1 = 0, t2 = 1, next = t1 + t2;
+	int i;
+
+	printf("%d, %d, ", t1, t2);
+
+	for (i = 3; i <= 50; i++)
+	{
+		printf("%d, ", next);
+		t1 = t2;
+		t2 = next;
+		next = t1 + t2;
+	}
+	printf("\n");
+	return (0);
+}
