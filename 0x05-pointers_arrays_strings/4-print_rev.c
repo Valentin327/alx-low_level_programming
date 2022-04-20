2328b77e5ff487e730fd5594da1e389fb31d052a
ValentinZONGO <valentizongo@gmail.com>
2022-04-20 15:34:49 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - function that prints a string, in reverse
+ * @s: parameter to reverse
+ * Return: no return
+ */
+void print_rev(char *s)
+{
+	int length;
+	int i;
+
+	while (*s != '\0')
+	{
+		length++;
+		*s++;
+	}
+	for (i = length; i >= 0; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
