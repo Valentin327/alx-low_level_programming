8913262d7df4ff6fb7b2b76b70559361e8decfc2
ValentinZONGO <valentizongo@gmail.com>
2022-04-20 14:31:47 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - function that return the length
+ * @s: parameter to check the length
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return length;
+}
