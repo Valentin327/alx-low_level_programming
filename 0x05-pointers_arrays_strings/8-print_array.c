56afe73c50b678adef759925b2fd59470feb366d
ValentinZONGO <valentizongo@gmail.com>
2022-04-21 06:57:52 -0700

    fifth commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,9 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		if (i == 0)
-			printf("%d\n", a[i]);
+			printf("%d", a[i]);
 		else
-			printf(", %d\n", a[i]); 
+			printf(", %d", a[i]);
 	}
 	printf("\n");
 }
