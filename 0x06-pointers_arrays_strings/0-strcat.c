7e0a32d961d77ee40ec91801c5daa5de6d634ca7
ValentinZONGO <valentizongo@gmail.com>
2022-04-22 06:43:54 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - function that concatenate two string
+ * @dest: string to concatenate
+ * @src: string that will add to dest string
+ * Return: return a char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	while (*dest)
+	{
+		dest++;
+	}
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (*dest);
+}
+
