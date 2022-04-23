80c753ea5384c848602d25964b86621da8cc08ff
ValentinZONGO <valentizongo@gmail.com>
2022-04-23 05:48:40 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy - function that copies a string.
+ * @dest: variable that copy other string
+ * @src: string to copy
+ * @n: n bytes of src to copy
+ * Return: return a string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+
+		for ( ; i < n; i++)
+		{
+			dest[i] = '\0';
+		}
+	}
+	return (dest);
+}
