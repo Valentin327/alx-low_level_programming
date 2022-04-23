d10e08a73bc8ab34d20f55563ad661fc6921fe99
ValentinZONGO <valentizongo@gmail.com>
2022-04-23 06:48:43 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: a string to compare with another
+ * @s2: another string to compare with the first string
+ * Return: return an integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int j;
+
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
