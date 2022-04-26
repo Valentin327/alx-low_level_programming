8eeb17107a2a62f20eb1f5b9ddf9188017857a45
ValentinZONGO <valentizongo@gmail.com>
2022-04-26 13:27:35 -0700

    initial commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - function that locates a character in a string
+ * @s: parameter that contain a string
+ * @c: a character in the string
+ * Return: return a pointer, c or Null
+ * */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+
+	while(1)
+	{
+		a = *s++;
+		if (a == c)
+		{
+			return (s - 1);
+		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+	}
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif
