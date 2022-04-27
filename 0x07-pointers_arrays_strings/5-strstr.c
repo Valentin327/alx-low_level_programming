c7cf614d903d422f2ced0a29601d9ac4e1145620
ValentinZONGO <valentizongo@gmail.com>
2022-04-27 05:57:33 -0700

    initial commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr - function that locates a substring
+ * @haystack: a string
+ * @needle: the first occurrence of the substring
+ * Return: return a 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (!needle[j])
+			return (&haystack[i]);
+	}
+	return (NULL);
+}
+
