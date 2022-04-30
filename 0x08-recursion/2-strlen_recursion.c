07eea9f53ebf40f3f52e0a0bc58edb367c0a42bf
ValentinZONGO <valentizongo@gmail.com>
2022-04-30 06:36:15 -0700

    initial commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function that return the length of a string
+ * @s: string to calcultae the length
+ * Return: 0 if string does not exist, the lenght otherwise
+ */
+int _strlen_recursion(char *s)
+{
+	if (! *s)
+	{
+		return (0);
+	}
+	else 
+	{
+		return (1 + _strlen_recursion(*s + 1);
+	}
+}
