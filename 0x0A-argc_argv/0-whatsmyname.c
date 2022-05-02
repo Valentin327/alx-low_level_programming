d0ef364f9861c8bc9f6ac9bbd016ff9f2dadb05f
ValentinZONGO <valentizongo@gmail.com>
2022-05-02 16:13:30 -0700

    second commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /** More headers goes there */
 
 /**
  * main - check the code
- *
+ * @argc: the number of program arguments
+ * @argv: an array that contain the value of arguments
  * Return: Always 0
  */
-int main(argc, argv[])
+int main(argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
