07cd66d895982196e1a5c38f26f0039aca3c432d
ValentinZONGO <valentizongo@gmail.com>
2022-05-02 16:43:59 -0700

    intial commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - checkt the code
+ * @argc: parameter that count the number of argument
+ * @argv: parameter tha contain the value of argument
+ * Return: Always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc -1);
+	return (0);
+}
