80c39b0b55650453646325444b0819b98fd21beb
ValentinZONGO <valentizongo@gmail.com>
2022-05-02 15:44:21 -0700

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/** More headers goes there */
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(argc, argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
