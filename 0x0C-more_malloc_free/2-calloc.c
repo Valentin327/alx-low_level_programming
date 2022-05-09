2ab57038f3df5b8c6a35020a4d07be7c978a7b91
ValentinZONGO <valentizongo@gmail.com>
2022-05-09 09:27:30 -0700

    fourth commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,10 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * _memset - fills memory with a constant byte
+ * _memset-fills memory with a constant byte
  * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
