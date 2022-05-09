01035ebd785c9a8b6b23a1df7054a925adeddad7
ValentinZONGO <valentizongo@gmail.com>
2022-05-09 15:09:27 -0700

    second commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-struct dog{
+
+/**
+ * struct dog - data structures (a avraiable that contain other variables)
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
