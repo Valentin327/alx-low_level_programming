b254f792b0232bffd53a0e9e769f04cf7e0cfd8d
ValentinZONGO <valentizongo@gmail.com>
2022-05-10 05:38:46 -0700

    initial commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,10 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
 #endif
