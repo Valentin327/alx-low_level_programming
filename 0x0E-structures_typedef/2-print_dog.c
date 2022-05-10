0afd23b55a9c79c323a36c86c981231b6361f1c9
ValentinZONGO <valentizongo@gmail.com>
2022-05-10 05:13:01 -0700

    third commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,10 +8,15 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+	{
+		return;
+	}
+
 	if (d->name == NULL)
-		printf("name: (nil)\n");
+		d->name = "(nil);
 	if (d->owner == NULL)
-		printf("owner: (nil)\n");
+		d->name = "(nil)";
 	printf("Name: %s\n", d->name);
 	printf("Age: %6f\n", d->age);
 	printf("Owner: %s\n", d->owner);
