0374cc4b814b6b17b88a34250934257a3b041c21
ValentinZONGO <valentizongo@gmail.com>
2022-05-09 17:23:44 -0700

    initial commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ * @struct dog: the data structure
+ * @name: first membere of structure
+ * @age: second member of structure
+ * @owner: third member of structure
+ *
+ * Return: no return
+ *
+ * Description: Longer description
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	
+	 d->name = name;
+	 d->age = age;
+	 d->owner = owner;
+}
