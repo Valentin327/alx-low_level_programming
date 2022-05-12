6c59d8a9e995d739193bd232131d2fa4adc177ad
ValentinZONGO <valentizongo@gmail.com>
2022-05-12 16:36:34 -0700

    second commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,17 +16,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		if (separator == NULL)
-		{
-			printf("%d ", va_arg(ap, int));
-		}
-		else
+		if (!separator)
 		{
 			printf("%d", va_arg(ap, int));
-			if (i < n - 1)
-			{
-				printf("%c ", *separator);
-			}
+		}
+		else if (separator && i == 0)
+		{
+			printf("%d", va_arg(ap, int));
+		}
+		else 
+		{
+			printf("%s%d", separator, va_arg(ap, int));
 		}
 	}
 	va_end(ap);
