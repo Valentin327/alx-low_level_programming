ca6f4e92fb3a11e98be72e18c923f09e0632be35
ValentinZONGO <valentizongo@gmail.com>
2022-05-13 16:15:11 -0700

    initial commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_name - function that print a name
+ * @name: pointer on the name
+ * @f: pointer to the printing function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+	{
+		return;
+	}
+	f(name);
+}
