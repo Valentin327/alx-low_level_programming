5130004a53a2b58583d03bf4a92cb356ef4b4a0c
ValentinZONGO <valentizongo@gmail.com>
2022-05-13 17:36:34 -0700

    second commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,5 @@
-#include "function_pointers.h"
 #include <stdlib.h>
-
+#include "function_pointers.h"
 /**
  * array_iterator - function that executes a function given as a parameter on
  * each element of an array
@@ -20,10 +19,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	}
 	else
 	{
-		for (i = 0; i < size, i++)
+		for (i = 0; i < size; i++)
 		{
 			action(array[i]);
 		}
 	}
 }
-
