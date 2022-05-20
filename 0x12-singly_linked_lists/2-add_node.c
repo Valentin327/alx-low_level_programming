8dfa98cd248e0cd5255df85cd50815c44209a53f
ValentinZONGO <valentizongo@gmail.com>
2022-05-20 10:58:26 -0700

    betty style update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,7 +7,6 @@
  * add_node - function that add a node at the beginning of a list
  * @head: Single linked list
  * @str: data contained in the structures
- * 
  * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
