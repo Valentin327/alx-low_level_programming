77d36f82618f2c0e887509d9aaa1cfd0362c7b99
ValentinZONGO <valentizongo@gmail.com>
2022-05-21 12:13:39 -0700

    third commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
-
+/**
+ * myStartupFun - function that execute
+ * before the main
+ * Reteturn: no return
+ */
 void myStartupFun(void) __attribute__ ((constructor));
 
 void myCleanupFun(void) __attribute__ ((destructor));
-
 void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
