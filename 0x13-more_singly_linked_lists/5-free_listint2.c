9df495354e7f75d0eee2dea7381a3dc19d94b5a5
ValentinZONGO <valentizongo@gmail.com>
2022-05-25 07:41:54 -0700

    initial commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a list
+ * @head: linked list
+ *
+ * Return: no return
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+
+	*head = NULL;
+}
