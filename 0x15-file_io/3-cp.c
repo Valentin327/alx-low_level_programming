e5ebf20b51614460b4dac743db39cca27fe9cc7b
ValentinZONGO <valentizongo@gmail.com>
2022-06-02 14:47:26 -0700

    initial commit

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * main - check the code
+ * @argc: arguments counter
+ * @argv: array for arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int ff_fd, ft_fd;
+	int c_ff, c_ft, r;
+	char buf[BUFSIZ];
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	ff_fd = open(argv[1], O_RDONLY);
+	if (ff_fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	ft_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((r = read(ff_fd, buf, BUFSIZ) > 0))
+	{
+		if (ft_fd == -1 || write(ft_fd, buf, r) != r)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	c_ff = close(ff_fd);
+	c_ft = close(ft_fd);
+	if (c_ff == -1 || c_ft == -1)
+	{
+		if (c_ff == -1)
+		{
+			dprintf(2, "Error: Can't close fd %d\n", ff_fd);
+		}
+		if (c_ft == -1)
+		{
+			dprintf(2, "Error: Can't close fd %d\n", ft_fd);
+		}
+		exit(100);
+	}
+	return (1);
+}
