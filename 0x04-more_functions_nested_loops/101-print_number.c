cc6f6c25ce30289eb82de9fc26399d0a91ec6bf6
Valentin ZONGO <valentizongo@gmail.com>
2022-06-16 16:09:16 +0000

    first update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,18 +7,19 @@
  */
 void print_number(int n)
 {
+	unsigned int num;
 	if (n < 0)
 	{
-		putchar('-');
-		n = -n;
+		num = -n
+		_putchar('-');
 	}
-	if (n == 0)
+	else
 	{
-		putchar('0');
+		num = n;
 	}
-	if (n / 10)
+	if (num / 10)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 	}
-	putchar(n % 10 + '0');
+	_putchar((num % 10) + '0');
 }
