47c023a8dc92b400f2fb8eb874dea2789477f155
Valentin ZONGO <valentizongo@gmail.com>
2022-06-17 00:11:05 +0000

    first update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,7 +10,7 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int ste;
+	unsigned long int set;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
--- 0x14-bit_manipulation/4-main.c
+++ 0x14-bit_manipulation/4-main.c
@@ -16,5 +16,8 @@ int main(void)
 	n = 0;
 	clear_bit(&n, 10);
 	printf("%lu\n", n);
+	n = 98;
+	clear_bit(&n, 1);
+	printf("%lu\n", n);
 	return (0);
 }
