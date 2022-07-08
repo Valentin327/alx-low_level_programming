7732dea3b259d38e6e53e940c79cdf6323c9f349
ValentinZONGO <valentizongo@gmail.com>
2022-07-08 10:04:41 -0700

    update file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_dlistint - function that prints all 
+ * print_dlistint - function that prints all
  * the elements of a dlistint_t list
  * @h: pointer to the start of the linked list
  *
@@ -12,7 +12,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
-
+	
 	for(i = 0; h != NULL; i++)
 	{
 		printf("%d\n", h->n);
