f2a30e964c486da3223ab364f7ff1938e7f13523
ValentinZONGO <valentizongo@gmail.com>
2022-07-11 15:15:34 -0700

    python and C

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+/**
+ * add - function that two numbers
+ * @a: number to add to another
+ * @b: number to add to another
+ *
+ * Return: sum of two number
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - function that perform subtraction
+ * of two number
+ * @a: a number
+ * @b: a number
+ *
+ * Return: result of difference
+ * two number
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * div - function to divide
+ * two number
+ * @a: a number
+ * @b: a number
+ *
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return ((int)(a / b));
+}
+/**
+ * mul - function to multiply
+ * two numbers
+ * @a: a number
+ * @b: a number
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * mod - function to perform
+ * modulo with two number
+ * @a: a number
+ * @b: a number
+ *
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
